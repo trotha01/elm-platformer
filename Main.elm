7cf75f6a3cc026486037c0012cf7a11ae20c01bb
trotha01 <trevor.rothaus@sendgrid.com>
2018-07-10 20:45:29 -0700

    wall collision

--- Main.elm
+++ Main.elm
@@ -10,19 +10,60 @@ import Time exposing (Time)
 type alias Model =
     { player : Player
     , pressedKeys : List Key
+    , collidables : List Rect
+    }
+
+
+type alias Rect =
+    -- (x,y) = top-left corner
+    { x : Int
+    , y : Int
+    , width : Int
+    , height : Int
     }
 
 
 type alias Player =
-    { x : Int
-    , y : Int
-    }
+    Rect
 
 
 initPlayer : Player
 initPlayer =
     { x = 10
     , y = 100
+    , width = 10
+    , height = 10
+    }
+
+
+( viewportHeight, viewportWidth ) =
+    ( 400, 600 )
+
+
+ground : Rect
+ground =
+    { x = 0
+    , y = viewportHeight
+    , width = viewportWidth
+    , height = 2
+    }
+
+
+leftWall : Rect
+leftWall =
+    { x = 0
+    , y = 0
+    , width = 2
+    , height = viewportHeight
+    }
+
+
+rightWall : Rect
+rightWall =
+    { x = viewportWidth
+    , y = 0
+    , width = 2
+    , height = viewportHeight
     }
 
 
@@ -30,6 +71,7 @@ init : ( Model, Cmd Msg )
 init =
     ( { player = initPlayer
       , pressedKeys = []
+      , collidables = [ ground, leftWall, rightWall ]
       }
     , Cmd.none
     )
@@ -51,8 +93,11 @@ update msg model =
             let
                 arrows =
                     Keyboard.arrows model.pressedKeys
+
+                player =
+                    updatePlayer delta arrows model.collidables model.player
             in
-            ( { model | player = updatePlayer delta arrows model.player }, Cmd.none )
+            ( { model | player = player }, Cmd.none )
 
         KeyMsg keyMsg ->
             ( { model | pressedKeys = Keyboard.update keyMsg model.pressedKeys }, Cmd.none )
@@ -63,16 +108,33 @@ speed =
     0.2
 
 
-updatePlayer : Time -> Arrows -> Player -> Player
-updatePlayer delta arrows player =
+updatePlayer : Time -> Arrows -> List Rect -> Player -> Player
+updatePlayer delta arrows rects player =
     let
-        x =
+        desiredX =
             player.x + round (delta * speed * toFloat arrows.x)
 
-        y =
+        desiredY =
             player.y + round (delta * speed * toFloat -arrows.y)
+
+        desiredPlayer =
+            { player | x = desiredX, y = desiredY }
+
+        isColliding =
+            List.any (collidesWith desiredPlayer) rects
     in
-    { x = x, y = y }
+    if isColliding then
+        player
+    else
+        desiredPlayer
+
+
+collidesWith : Rect -> Rect -> Bool
+collidesWith rect1 rect2 =
+    (rect1.x < rect2.x + rect2.width)
+        && (rect1.x + rect1.width > rect2.x)
+        && (rect1.y < rect2.y + rect2.height)
+        && (rect1.height + rect1.y > rect2.y)
 
 
 view : Model -> Html Msg
@@ -85,7 +147,9 @@ view model =
             , ( "border", "1px solid black" )
             ]
         ]
-        [ viewPlayer model.player ]
+        [ viewPlayer model.player
+        , div [] (List.map viewCollidable model.collidables)
+        ]
 
 
 viewPlayer : Player -> Html Msg
@@ -99,6 +163,22 @@ viewPlayer player =
             , ( "background", "lightblue" )
             , ( "left", px player.x )
             , ( "top", px player.y )
+            ]
+        ]
+        []
+
+
+viewCollidable : Rect -> Html Msg
+viewCollidable rect =
+    div
+        [ style
+            [ ( "position", "absolute" )
+            , ( "width", px rect.width )
+            , ( "height", px rect.height )
+            , ( "border", "1px solid black" )
+            , ( "background", "gray" )
+            , ( "left", px rect.x )
+            , ( "top", px rect.y )
             ]
         ]
         []
